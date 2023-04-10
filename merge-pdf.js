c1cc43b81a363ff1b471f0a1043da7d471bb513b
nitin31122002 <b.nitinchowdary@gmail.com>
2023-04-10 17:21:18 +0530

    Added delete pages feature

--- merge-pdf.js
+++ merge-pdf.js
@@ -2,13 +2,30 @@ const express = require('express');
 const { PDFDocument } = require('pdf-lib');
 const multer = require('multer');
 const fs = require('fs').promises;
+const tmp = require('tmp');
 const path = require('path');
 const PORT = process.env.PORT || 3030;
 
 const app = express();
 app.use(express.static("public"));
-const upload = multer({ dest: 'uploads/' });
-
+app.use(express.json());
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      const tmpDir = tmp.dirSync({ mode: '755', prefix: 'pdf-' });
+      cb(null, tmpDir.name);
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    if (!file.originalname.match(/\.(pdf)$/)) {
+      return cb(new Error('Please upload only PDF files.'));
+    }
+    cb(null, true);
+  }
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -50,21 +67,75 @@ app.post('/merge', upload.array('pdf-files'), async (req, res) => {
   }
 });
 
+app.post('/delete-pages', upload.single('pdf-file'), async (req, res) => {
+  const file = req.file.path;
+  const pagesToDelete = req.body.pagesToDelete.split(',').map(Number);
+
+  if (!pagesToDelete || !pagesToDelete.length) {
+    return res.status(400).send('No pages selected for deletion');
+  }
+
+  try {
+    const modifiedPdf = await deletePagesFromPDF(file, pagesToDelete);
+    const fileName = 'modified.pdf';
+    const filePath = path.join(__dirname, fileName);
+
+    await fs.writeFile(filePath, modifiedPdf);
+    await fs.unlink(file);
+    res.download(filePath, fileName, (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Error downloading modified PDF');
+      }
+
+      fs.unlink(filePath, (err) => {
+        if (err) console.error(err);
+      });
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error deleting pages from PDF');
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
 async function mergePDFs(files) {
   const mergedPdf = await PDFDocument.create();
 
   for (const file of files) {
     const pdfBytes = await fs.readFile(file);
     const pdfDoc = await PDFDocument.load(pdfBytes);
-
     const copiedPages = await mergedPdf.copyPages(pdfDoc, pdfDoc.getPageIndices());
     copiedPages.forEach((page) => mergedPdf.addPage(page));
   }
 
-  const pdfBytes = await mergedPdf.save();
-  return pdfBytes;
+  return await mergedPdf.save();
 }
 
-app.listen(PORT, () => {
-  console.log('Server started on http://localhost:3030');
-});
+
+async function deletePagesFromPDF(pdfPath, pagesToDelete) {
+  const pdfDoc = await PDFDocument.load(await fs.readFile(pdfPath));
+  const deletedPages = [];
+
+  for (let i = pagesToDelete.length - 1; i >= 0; i--) {
+    const pageNum = pagesToDelete[i];
+    const index = pageNum - 1;
+    const page = pdfDoc.getPages()[index];
+    if (page) {
+      pdfDoc.removePage(index);
+      deletedPages.push(index + 1);
+    }
+  }
+  const modifiedPdf = await pdfDoc.save();
+  console.log(`Deleted pages ${deletedPages.join(', ')}`);
+  return modifiedPdf;
+}
+
+
+module.exports = {
+  deletePagesFromPDF
+};
+
